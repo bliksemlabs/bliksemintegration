ce02be319b7c2c41d89083347e1aba976ccbc3bf
Skywave <thakoch@gmail.com>
2013-12-06 14:10:52 +0100

    Add manual importers

---
+++ qbuzz-import.py
@@ -0,0 +1,18 @@
+import importers.qbuzz
+import logging
+import sys
+
+logger = logging.getLogger("importer")
+fh = logging.FileHandler('error.log')
+fh.setLevel(logging.ERROR)
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.INFO)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+path = '/home/thomas/rid'
+filename = sys.argv[1]
+meta = {'dataownercode' : 'QBUZZ'}
+
+importers.qbuzz.import_zip(path,filename,meta)
---
+++ retbus-import.py
@@ -0,0 +1,18 @@
+import importers.retbus
+import logging
+import sys
+
+logger = logging.getLogger("importer")
+fh = logging.FileHandler('error.log')
+fh.setLevel(logging.ERROR)
+ch = logging.StreamHandler(stream=sys.stdout)
+ch.setLevel(logging.INFO)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+path = '/home/thomas/rid/'
+filename = sys.argv[1]
+meta = {'dataownercode' : 'RET'}
+
+importers.retbus.import_zip(path,filename,meta)
