567e428ac4b51fad1e5691db7f29ed257379f77a
skywave <skywave@users.noreply.github.com>
2015-07-11 10:37:55 +0200

    Update retbus.py

--- importers/retbus.py
+++ importers/retbus.py
@@ -62,7 +62,8 @@ def import_zip(path,filename,version):
         return
     try:
         fixBob(conn)
-        generatePool(conn)
+        if pool_generation_enabled:
+            generatePool(conn)
         data = {}
         data['OPERATOR'] = getOperator()
         data['MERGESTRATEGY'] = getMergeStrategies(conn)
