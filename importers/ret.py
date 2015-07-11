69604bb6965b37a344a092f9e1b3058297b788d8
skywave <skywave@users.noreply.github.com>
2015-07-11 10:37:15 +0200

    Update ret.py

--- importers/ret.py
+++ importers/ret.py
@@ -190,7 +190,8 @@ def import_zip(path,filename,version):
     try:
         fixBob(conn)
         cleanDest(conn)
-        generatePool(conn)
+        if pool_generation_enabled:
+            generatePool(conn)
         data = {}
         data['OPERATOR'] = getOperator()
         data['MERGESTRATEGY'] = getMergeStrategies(conn)
